31c649ec8e236a87fb25bdc6ea64b6d42a4cbf03
Tedrick <tedrikex@gmail.com>
2025-01-28 15:10:25 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
+use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
+
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        Route::get('/', function () {
+            return view('welcome');
+        });
+    });
+}
